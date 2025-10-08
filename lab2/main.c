bb36f37fc33e964f113f371f2a10aabd3b41100e
Delphington <89201997+Delphington@users.noreply.github.com>
2025-10-08 23:08:19 +0300

    dev lab2

--- lab2/main.c
+++ lab2/main.c
@@ -1,3 +1,4 @@
+#define _GNU_SOURCE
 #include <stdio.h>
 #include <unistd.h>
 #include <sys/stat.h>
@@ -12,297 +13,347 @@
 #include <limits.h>
 #include <getopt.h>
 
-
-enum ListFlags
-{
+// Перечисления для флагов, цветов и кодов ошибок
+typedef enum {
     LS_ALL = 1,
     LS_LONG = 2
-};
+} ls_flags_t;
 
-enum FileColor
-{
-    COL_FILE,
-    COL_DIR,
-    COL_EXEC,
-    COL_LN
-};
+typedef enum {
+    COLOR_FILE,
+    COLOR_DIR,
+    COLOR_EXEC,
+    COLOR_LINK
+} file_color_t;
 
-enum ErrorCode
-{
-    ERR_NEARGS,
-    ERR_INVALIDOPT,
-    ERR_FILE_ISNT_SPEC,
-    ERR_OPENDIR,
-    ERR_READDIR,
+typedef enum {
+    ERR_NOT_ENOUGH_ARGS,
+    ERR_INVALID_OPTION,
+    ERR_FILE_NOT_SPECIFIED,
+    ERR_OPEN_DIR,
+    ERR_READ_DIR,
     ERR_STAT,
-    ERR_PWD,
-    ERR_GRP
-};
+    ERR_GET_USER,
+    ERR_GET_GROUP
+} error_code_t;
 
-static const int FILE_TYPE_COLORS[] = {
-    39,  // файлы
-    34,  // Синий (директории)
-    32,  // Зеленый (исполняемые файлы)
-    36   // Бирюзовый (символические ссылки)
-};
-
+static const int COLOR_CODES[] = {39, 34, 32, 36}; // Белый, Синий, Зеленый, Бирюзовый
 static const char * const VALID_OPTIONS = "hla";
 static const char * const PERMISSION_CHARS = "rwx";
 static const int TIME_STRING_OFFSET = 4;
 static const int TIME_STRING_LENGTH = 12;
+static const int MAX_PATH_SIZE = 4096;
+
 // ANSI escape последовательности
 static const char * const ANSI_RESET = "\x1b[0m";
 static const char * const ANSI_COLOR_PREFIX = "\x1b[;";
-const char * g_basePath = NULL;
-static const size_t MAX_PATH_LENGTH =
-#ifdef PATH_MAX
-    (size_t)PATH_MAX
-#else
-    (size_t)4096
-#endif
-;
-char * g_pathBuffer = NULL;
 
-DIR *g_openDir = NULL;
-size_t g_entryCount = 0;
-struct dirent ** g_entries = NULL;
-int g_flags = 0;
+// Глобальные переменные
+static const char *base_path = NULL;
+static char *path_buffer = NULL;
+static DIR *open_dir = NULL;
+static size_t entry_count = 0;
+static struct dirent **entries = NULL;
+static int flags = 0;
 
-void initProgram();
-void failWithError(enum ErrorCode);
-void composePath(const char * fileName);
-void collectEntries();
-void printEntry(struct dirent* entry);
-void listDirectory(const char *dirPath);
-void closeDirOnExit();
-void freePathOnExit();
-void freeEntryListOnExit();
-int direntCompare(const void* a, const void* b);
+// Прототипы функций
+static void init_program(void);
+static void cleanup_and_exit(error_code_t error);
+static void compose_path(const char *filename);
+static void collect_entries(void);
+static void print_entry(struct dirent *entry);
+static void list_directory(const char *dir_path);
+static void close_dir_on_exit(void);
+static void free_path_on_exit(void);
+static void free_entries_on_exit(void);
+static int compare_entries(const void *a, const void *b);
 
+int main(int argc, char **argv)
+{
+    init_program();
 
-int main(int argc, char **argv){
-    initProgram();
-
-    opterr = 0; // подавить сообщения об ошибках getopt
+    // Парсинг опций
+    opterr = 0; // Отключить сообщения об ошибках getopt
     int option;
-    while ((option = getopt(argc, argv, VALID_OPTIONS)) != -1){
-        switch (option){
+    while ((option = getopt(argc, argv, VALID_OPTIONS)) != -1) {
+        switch (option) {
             case 'h':
-                printf("ls - list directory contents\n"
-                       "usage: ls [params...] [file]\n"
-                       " -a - do not ignore entries starting with '.'\n"
-                       " -l - use a long listing format\n"
-                       " -h - print this message\n");
+                printf("ls - список содержимого директории\n"
+                       "использование: ls [параметры...] [файл]\n"
+                       " -a - не игнорировать записи, начинающиеся с '.'\n"
+                       " -l - использовать длинный формат списка\n"
+                       " -h - показать это сообщение\n");
                 exit(EXIT_SUCCESS);
             case 'l':
-                g_flags |= LS_LONG;
+                flags |= LS_LONG;
                 break;
             case 'a':
-                g_flags |= LS_ALL;
+                flags |= LS_ALL;
                 break;
             default:
-                failWithError(ERR_INVALIDOPT);
+                cleanup_and_exit(ERR_INVALID_OPTION);
         }
     }
 
     // Определение целевой директории
-    const char *targetDir = (optind == argc) ? "." : argv[optind];
-    listDirectory(targetDir);
+    const char *target_dir = (optind == argc) ? "." : argv[optind];
+    list_directory(target_dir);
     exit(EXIT_SUCCESS);
 }
 
-void initProgram(){
-    g_pathBuffer = (char *)malloc(MAX_PATH_LENGTH);
-    atexit(freePathOnExit);
+static void init_program(void)
+{
+    path_buffer = malloc(MAX_PATH_SIZE);
+    if (!path_buffer) {
+        fprintf(stderr, "[ls]: Ошибка выделения памяти\n");
+        exit(EXIT_FAILURE);
+    }
+    atexit(free_path_on_exit);
 }
 
-void failWithError(enum ErrorCode err){
-    switch (err){
-    case ERR_NEARGS:
-        fprintf(stderr, "[ls]: Not enough args! Usage: ls -h\n");
-        break;
-    case ERR_INVALIDOPT:
-        fprintf(stderr, "[ls]: Error: invalid option, see \"ls -h\"\n");
-        break;
-    case ERR_FILE_ISNT_SPEC:
-        fprintf(stderr, "[ls]: Error! File isn't specified\n");
-        break;
-    case ERR_OPENDIR:
-        fprintf(stderr, "[ls]: Error while opening directory! %s\n",
-                strerror(errno));
-        break;
-    case ERR_READDIR:
-        fprintf(stderr, "[ls]: Error while parsing files! %s\n",
-                strerror(errno));
-        break;
-    case ERR_STAT:
-        fprintf(stderr, "[ls]: error while getting stat! %s\n",
-                strerror(errno));
-        break;
-    case ERR_PWD:
-        fprintf(stderr, "[ls]: error while getting username! %s\n",
-                strerror(errno));
-        break;
-    case ERR_GRP:
-        fprintf(stderr, "[ls]: error while getting groupname! %s\n",
-                strerror(errno));
-        break;
+static void cleanup_and_exit(error_code_t error)
+{
+    switch (error) {
+        case ERR_NOT_ENOUGH_ARGS:
+            fprintf(stderr, "[ls]: Недостаточно аргументов! Использование: ls -h\n");
+            break;
+        case ERR_INVALID_OPTION:
+            fprintf(stderr, "[ls]: Ошибка: неверная опция, см. \"ls -h\"\n");
+            break;
+        case ERR_FILE_NOT_SPECIFIED:
+            fprintf(stderr, "[ls]: Ошибка! Файл не указан\n");
+            break;
+        case ERR_OPEN_DIR:
+            fprintf(stderr, "[ls]: Ошибка при открытии директории! %s\n", strerror(errno));
+            break;
+        case ERR_READ_DIR:
+            fprintf(stderr, "[ls]: Ошибка при чтении файлов! %s\n", strerror(errno));
+            break;
+        case ERR_STAT:
+            fprintf(stderr, "[ls]: Ошибка при получении информации о файле! %s\n", strerror(errno));
+            break;
+        case ERR_GET_USER:
+            fprintf(stderr, "[ls]: Ошибка при получении имени пользователя! %s\n", strerror(errno));
+            break;
+        case ERR_GET_GROUP:
+            fprintf(stderr, "[ls]: Ошибка при получении имени группы! %s\n", strerror(errno));
+            break;
     }
     exit(EXIT_FAILURE);
 }
 
 // Построение полного пути из директории и имени файла
-void composePath(const char * fileName){
-    if (g_basePath == NULL){
-        g_basePath = ".";
+static void compose_path(const char *filename)
+{
+    if (base_path == NULL) {
+        base_path = ".";
     }
-    int written = snprintf(g_pathBuffer, MAX_PATH_LENGTH, "%s/%s", g_basePath, fileName);
-    if (written < 0 || (size_t)written >= MAX_PATH_LENGTH){
-        fprintf(stderr, "[ls]: path too long\n");
+    int written = snprintf(path_buffer, MAX_PATH_SIZE, "%s/%s", base_path, filename);
+    if (written < 0 || (size_t)written >= MAX_PATH_SIZE) {
+        fprintf(stderr, "[ls]: Путь слишком длинный\n");
         exit(EXIT_FAILURE);
     }
 }
 
 // Сбор и сортировка записей директории
-void collectEntries(){
+static void collect_entries(void)
+{
     errno = 0;
-    for (struct dirent *cur_file; (cur_file = readdir(g_openDir)) != NULL;
-         g_entryCount++);
-    if (errno){
-        failWithError(ERR_READDIR);
+    size_t total_blocks = 0;
+    
+    // Первый проход: подсчет записей и блоков
+    for (struct dirent *cur_file; (cur_file = readdir(open_dir)) != NULL; entry_count++) {
+        if ((flags & LS_ALL) || cur_file->d_name[0] != '.') {
+            struct stat temp;
+            compose_path(cur_file->d_name);
+            if (lstat(path_buffer, &temp) == 0) {
+                total_blocks += temp.st_blocks;
+            }
+        }
     }
-    g_entries = (struct dirent **)malloc(g_entryCount * sizeof(struct dirent *));
-    atexit(freeEntryListOnExit);
+    
+    if (errno) {
+        cleanup_and_exit(ERR_READ_DIR);
+    }
+    
+    entries = malloc(entry_count * sizeof(struct dirent *));
+    if (!entries) {
+        fprintf(stderr, "[ls]: Ошибка выделения памяти\n");
+        exit(EXIT_FAILURE);
+    }
+    atexit(free_entries_on_exit);
 
-    rewinddir(g_openDir);
+    rewinddir(open_dir);
     int i = 0;
-    for (struct dirent *cur_file; (cur_file = readdir(g_openDir)) != NULL;
-         g_entries[i++] = cur_file);
-    qsort(g_entries, g_entryCount, sizeof(struct dirent *), direntCompare);
+    for (struct dirent *cur_file; (cur_file = readdir(open_dir)) != NULL; entries[i++] = cur_file);
+    
+    qsort(entries, entry_count, sizeof(struct dirent *), compare_entries);
+    
+    // Вывод total для длинного формата
+    if (flags & LS_LONG) {
+        printf("total %zu\n", total_blocks / 2);
+    }
 }
 
 // Вывод одной записи директории
-void printEntry(struct dirent *entry){
-    if (entry->d_name[0] == '.' && !(g_flags & LS_ALL)){
+static void print_entry(struct dirent *entry)
+{
+    if (entry->d_name[0] == '.' && !(flags & LS_ALL)) {
         return;
     }
-    composePath(entry->d_name);
-    enum FileColor filename_color = COL_FILE;
+    
+    compose_path(entry->d_name);
+    file_color_t filename_color = COLOR_FILE;
     struct stat file_info;
-    if (lstat(g_pathBuffer, &file_info) == -1) {
-        // printf("%s\n", g_pathBuffer);
-        failWithError(ERR_STAT);
+    
+    if (lstat(path_buffer, &file_info) == -1) {
+        cleanup_and_exit(ERR_STAT);
     }
 
-    if (g_flags & LS_LONG){
+    if (flags & LS_LONG) {
         // Определение типа файла и установка цвета
-        char fileTypeChar = '?';
-        if (S_ISREG(file_info.st_mode)){
-            fileTypeChar = '-';
-            if (file_info.st_mode & S_IXUSR){
-                filename_color = COL_EXEC;
+        char file_type_char = '?';
+        if (S_ISREG(file_info.st_mode)) {
+            file_type_char = '-';
+            if (file_info.st_mode & S_IXUSR) {
+                filename_color = COLOR_EXEC;
             }
+        } else if (S_ISDIR(file_info.st_mode)) {
+            file_type_char = 'd';
+            filename_color = COLOR_DIR;
+        } else if (S_ISBLK(file_info.st_mode)) {
+            file_type_char = 'b';
+        } else if (S_ISLNK(file_info.st_mode)) {
+            file_type_char = 'l';
+            filename_color = COLOR_LINK;
         }
-        else if (S_ISDIR(file_info.st_mode)){
-            fileTypeChar = 'd';
-            filename_color = COL_DIR;
-        }
-        else if (S_ISBLK(file_info.st_mode)){
-            fileTypeChar = 'b';
-        }
-        else if (S_ISLNK(file_info.st_mode)){
-            fileTypeChar = 'l';
-            filename_color = COL_LN;
-        }
-        putchar(fileTypeChar);
+        putchar(file_type_char);
 
-        // права доступа к файлу
+        // Права доступа к файлу
         int i = 0;
-        for (uint64_t mask = S_IRUSR; mask > 0; mask >>= 1, i++){
-            putchar(file_info.st_mode & mask ? PERMISSION_CHARS[i%3] : '-');
+        for (uint64_t mask = S_IRUSR; mask > 0; mask >>= 1, i++) {
+            putchar(file_info.st_mode & mask ? PERMISSION_CHARS[i % 3] : '-');
         }
         putchar(' ');
 
+        // Получение имени пользователя
         errno = 0;
         struct passwd *pwd_file = getpwuid(file_info.st_uid);
-        if (pwd_file == NULL && errno){
-            failWithError(ERR_PWD);
+        if (pwd_file == NULL && errno) {
+            cleanup_and_exit(ERR_GET_USER);
         }
 
-        // valgrind сообщает об утечке памяти здесь
+        // Получение имени группы
         errno = 0;
         struct group *grp_file = getgrgid(file_info.st_gid);
-        if (grp_file == NULL && errno){
-            failWithError(ERR_GRP);
+        if (grp_file == NULL && errno) {
+            cleanup_and_exit(ERR_GET_GROUP);
         }
 
-        // жесткие ссылки, группы, размер
-        printf("%lu %s %s %lu ", file_info.st_nlink, pwd_file->pw_name, grp_file->gr_name, file_info.st_size);
+        // Количество ссылок, пользователь, группа, размер
+        printf("%lu %s %s %lu ", file_info.st_nlink, 
+               pwd_file ? pwd_file->pw_name : "unknown",
+               grp_file ? grp_file->gr_name : "unknown", 
+               file_info.st_size);
 
-        // время
+        // Время модификации
         char *time_str = ctime(&file_info.st_mtime);
         char output_time_str[TIME_STRING_LENGTH + 1];
         strncpy(output_time_str, time_str + TIME_STRING_OFFSET, TIME_STRING_LENGTH);
         output_time_str[TIME_STRING_LENGTH] = '\0';
         printf("%s ", output_time_str);
 
-        // имя файла: "несколько слов" -> `несколько слов`
-        if (strchr(entry->d_name, ' ') != NULL){
-            printf("%s%dm`%s`%s\n", ANSI_COLOR_PREFIX, FILE_TYPE_COLORS[filename_color],
+        // Имя файла с цветом
+        if (strchr(entry->d_name, ' ') != NULL) {
+            printf("%s%dm`%s`%s", ANSI_COLOR_PREFIX, COLOR_CODES[filename_color],
+                   entry->d_name, ANSI_RESET);
+        } else {
+            printf("%s%dm%s%s", ANSI_COLOR_PREFIX, COLOR_CODES[filename_color],
                    entry->d_name, ANSI_RESET);
         }
-        else{
-            printf("%s%dm%s%s\n", ANSI_COLOR_PREFIX, FILE_TYPE_COLORS[filename_color],
+
+        // Для символических ссылок показать цель
+        if (S_ISLNK(file_info.st_mode)) {
+            size_t bufsize = (file_info.st_size > 0) ? file_info.st_size + 1 : PATH_MAX;
+            char *buf = malloc(bufsize);
+            if (!buf) {
+                cleanup_and_exit(ERR_STAT);
+            }
+            int len = readlink(path_buffer, buf, bufsize - 1);
+            if (len == -1) {
+                free(buf);
+                cleanup_and_exit(ERR_STAT);
+            }
+            buf[len] = '\0';
+            printf(" -> %s", buf);
+            free(buf);
+        }
+        putchar('\n');
+    } else {
+        // Короткий формат
+        if (S_ISREG(file_info.st_mode) && (file_info.st_mode & S_IXUSR)) {
+            filename_color = COLOR_EXEC;
+        } else if (S_ISDIR(file_info.st_mode)) {
+            filename_color = COLOR_DIR;
+        } else if (S_ISLNK(file_info.st_mode)) {
+            filename_color = COLOR_LINK;
+        }
+
+        // Имя файла с цветом
+        if (strchr(entry->d_name, ' ') != NULL) {
+            printf("%s%dm`%s`%s  ", ANSI_COLOR_PREFIX, COLOR_CODES[filename_color], 
                    entry->d_name, ANSI_RESET);
-        }
-    } else{
-        if (S_ISREG(file_info.st_mode) && (file_info.st_mode & S_IXUSR)){
-            filename_color = COL_EXEC;
-        }
-        else if (S_ISDIR(file_info.st_mode)){
-            filename_color = COL_DIR;
-        }
-        else if (S_ISLNK(file_info.st_mode)){
-            filename_color = COL_LN;
-        }
-        // имя файла: "несколько слов" -> `несколько слов`
-        if (strchr(entry->d_name, ' ') != NULL){
-            printf("%s%dm`%s`%s  ", ANSI_COLOR_PREFIX, FILE_TYPE_COLORS[filename_color], entry->d_name, ANSI_RESET);
-        }
-        else{
-            printf("%s%dm%s%s  ", ANSI_COLOR_PREFIX, FILE_TYPE_COLORS[filename_color], entry->d_name, ANSI_RESET);
+        } else {
+            printf("%s%dm%s%s  ", ANSI_COLOR_PREFIX, COLOR_CODES[filename_color], 
+                   entry->d_name, ANSI_RESET);
         }
     }
 }
 
-// Основная функция вывода содержимого директории
-void listDirectory(const char *dirPath){
-    g_openDir = opendir(dirPath);
-    if (g_openDir == NULL){
-        failWithError(ERR_OPENDIR);
+static void list_directory(const char *dir_path)
+{
+    open_dir = opendir(dir_path);
+    if (open_dir == NULL) {
+        cleanup_and_exit(ERR_OPEN_DIR);
     }
 
-    atexit(closeDirOnExit);
-    g_basePath = dirPath;
-    // сканирование директорий
-    errno = 0; // для обнаружения ошибки в readdir()
-    collectEntries();
-    for (size_t i = 0; i < g_entryCount; ++i){
-        printEntry(g_entries[i]);
+    atexit(close_dir_on_exit);
+    base_path = dir_path;
+    
+    errno = 0;
+    collect_entries();
+    
+    for (size_t i = 0; i < entry_count; ++i) {
+        print_entry(entries[i]);
     }
-    // после вывода файлов без флага LS_LONG нет '\n' в конце
-    if (!(g_flags & LS_LONG)){
+    
+    if (!(flags & LS_LONG)) {
         putchar('\n');
     }
 }
 
-// Функции очистки
-void closeDirOnExit() { if (g_openDir) { closedir(g_openDir); g_openDir = NULL; } }
-void freePathOnExit() { free(g_pathBuffer); }
-void freeEntryListOnExit() { free(g_entries); }
+// Функции очистки ресурсов
+static void close_dir_on_exit(void) 
+{ 
+    if (open_dir) { 
+        closedir(open_dir); 
+        open_dir = NULL; 
+    } 
+}
 
-// Функция сравнения для сортировки
-int direntCompare(const void *a, const void *b){
-    struct dirent *f = *(struct dirent **)a, *s = *(struct dirent **)b;
-    return strcmp(f->d_name, s->d_name);
+static void free_path_on_exit(void) 
+{ 
+    free(path_buffer); 
+}
+
+static void free_entries_on_exit(void) 
+{ 
+    free(entries); 
+}
+
+static int compare_entries(const void *a, const void *b)
+{
+    struct dirent *first = *(struct dirent **)a;
+    struct dirent *second = *(struct dirent **)b;
+    return strcmp(first->d_name, second->d_name);
 }
